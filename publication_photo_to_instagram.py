723ad96b5b62b36501b53bd94bff117324d189d3
acev3 <kovalchukilya5@gmail.com>
2020-12-08 15:47:50 +0300

    Игнорирование лишнего файла и испрвление работы с путями

--- publication_photo_to_instagram.py
+++ publication_photo_to_instagram.py
@@ -1,9 +1,12 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 from instabot import Bot
 
 
+
 def main():
+    IMAGES_DIR = Path.cwd().joinpath('images')
     load_dotenv()
     INSTAGRAM_USER_NAME = os.environ['INSTAGRAM_USER_NAME']
     INSTAGRAM_PASSWORD = os.environ['INSTAGRAM_PASSWORD']
@@ -11,10 +14,11 @@ def main():
     bot.login(username=INSTAGRAM_USER_NAME, password=INSTAGRAM_PASSWORD)
     images = os.listdir("images")
     for image in images:
-        if bot.upload_photo("images/{}".format(image), caption="Nice pic!"):
-            os.remove("images/{}.REMOVE_ME".format(image))
+        image_path = IMAGES_DIR.joinpath(image)
+        if bot.upload_photo(image_path, caption="Nice pic!"):
+            os.remove("{}.REMOVE_ME".format(image_path))
         else:
-            os.remove("images/{}".format(image))
+            os.remove(image_path)
 
 
 if __name__ == '__main__':
